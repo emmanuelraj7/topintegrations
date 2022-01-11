dde19f1b898897c46f92ab1f476bef0d006a237d
Emmanuel Raj <emmanuelraj7@gmail.com>
2022-01-12 02:17:15 +0530

    updated frontpage

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Tailwind CSS Style Sheet
 import 'assets/styles/tailwind.css';
+import About from 'components/About';
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
             <Route exact path="/profile" component={Profile} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
+            <Route path="/about" component={About}/>
             <Redirect from="*" to="/" />
         </Switch>
     );
